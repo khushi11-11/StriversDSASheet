2c120bca26b739aaf724076a55e5d066fc2ff2ee
Khushi Verma <khushi7991012@gmail.com>
2025-06-05 21:10:27 +0530

    Array easy question added

---
+++ src/array/easy/Q11_MaxConsecutive1s.java
@@ -0,0 +1,43 @@
+package array.easy;
+
+import java.util.ArrayList;
+
+/*
+COUNT MAXIMUM CONSECUTIVE ONE'S IN THE ARRAY~
+Problem Statement: Given an array that contains only 1 and 0 return the count of maximum consecutive ones in the array.
+Example 1:
+Input: prices = {1, 1, 0, 1, 1, 1}
+Output: 3
+Explanation: There are two consecutive 1’s and three consecutive 1’s in the array out of which maximum is 3.
+Input: prices = {1, 0, 1, 1, 0, 1}
+Output: 2
+Explanation: There are two consecutive 1's in the array.
+ */
+public class Q11_MaxConsecutive1s {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 1, 0, 1, 1, 1};
+        int ans = result(arr1);
+        System.out.println(ans);
+    }
+
+    static int result(int[] arr1) {
+        ArrayList<Integer> counter = new ArrayList<>();
+        int count=0;
+        for (int i = 0; i < arr1.length; i++) {
+            if(arr1[i]==1 && i!=arr1.length-1) {
+                count++;
+            } else if (arr1[i]==1 && i==arr1.length-1) {
+                count++;
+                counter.add(count);
+            } else {
+                counter.add(count);
+                count=0;
+            }
+        }
+        int max = Integer.MIN_VALUE;
+        for (int i : counter) {
+            if (max<i) max=i;
+        }
+        return max;
+    }
+}
