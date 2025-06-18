3c584e6fcd2ac27366f90fac4ebae4b371bcc4df
Khushi Verma <khushi7991012@gmail.com>
2025-06-18 15:30:30 +0530

    updates

--- src/array/daily/SubarrayWithSumK.java
+++ src/array/daily/SubarrayWithSumK.java
@@ -1,16 +1,17 @@
 package array.daily;
 
-import java.util.ArrayList;
-
 public class SubarrayWithSumK {
     public static void main(String[] args) {
         int[] arr1 = {10, 5, 2, 7, 1, 9};
+        int[] arr2 = {9, 1, 7, 2, 5, 10};
         int target = 15;
         solution(arr1, target);
+        solution(arr2, target);
     }
     static void solution(int[] nums, int k) {
-        int left=0, right=0, sum=0, len=0;
-        int start=0, end=0;
+        int left=0, right=0; // for window
+        int sum=0, len=0; // for tracking sum and max length of subarray
+        int start=0, end=0; // for tracking index of subarray to print it
         while (right < nums.length) {
             sum += nums[right];
             if (sum > k) {
@@ -18,10 +19,12 @@ public class SubarrayWithSumK {
                 left++;
             }
             if (sum == k) {
-                len = Math.max(len, right-left+1);// +1 bcz initially (when loop starts) left and right is the same but still adds up to sum
-                start = left;
-                end = right;
-
+//                len = Math.max(len, right-left+1);// +1 bcz initially (when loop starts) left and right is the same but still adds up to sum
+                if (len < right-left+1) {
+                    start = left;
+                    end = right;
+                    len = right-left+1;
+                }
             }
             right++;
         }
