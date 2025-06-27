38e8f5db5f3f9034ff02d3ba33aa873d80f4a8a0
Khushi Verma <khushi7991012@gmail.com>
2025-06-27 15:06:50 +0530

    optimal approach + typical optimal code

--- src/array/medium/Q10_LongestSubsequence.java
+++ src/array/medium/Q10_LongestSubsequence.java
@@ -1,6 +1,7 @@
 package array.medium;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 /*
 LONGEST CONSECUTIVE SEQUENCE IN AN ARRAY~
@@ -17,8 +18,14 @@ public class Q10_LongestSubsequence {
     public static void main(String[] args) {
         int[] arr1 = {100, 200, 1, 3, 2, 4};
         int[] arr2 = {3, 8, 5, 7, 6};
-        bruteForce(arr1);
-        bruteForce(arr2);
+        int[] arr3 = {9, 1, -3, 2, 4, 8, 3, -1, 6, -2, -4, 7};
+
+        int[] test1 = {10, 30, 20, 21, 22, 50};
+        int[] test2 = {1, 2, 3, 4, 5, 100, 200, 3, 2, 1};
+        int[] test3 = {5, 100, 4, 3, 2, 1, 6};
+        optimal(test1);
+        optimal(test2);
+        optimal(test3);
     }
     static void bruteForce(int[] nums) {
         Arrays.sort(nums);
@@ -35,6 +42,56 @@ public class Q10_LongestSubsequence {
             } else {
                 count=1; // reset counter
                 ptr++;
+            }
+        }
+        System.out.println(max);
+    } // time=nlogn, space=n
+
+    static void optimal(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int x : nums) set.add(x);
+        int start=0, incr =1;
+        int count=1, max=count;
+        /*
+        1. finding start point of a seq-> whose -1 does not exist
+            2. when found -> start doesn't change, checks for next value (+1)
+                2-a. if its successor exists -> seq+1, count+1, update max
+                2-b. if successor not found (of seq-ptr) -> count=1, start gets updated to curr seq ptr
+            3. if not found -> incr start, update seq ptr
+         */
+        while (start < nums.length){
+            if (set.contains(nums[start]-1)) { // not start (-1 exists)
+                start++;
+            } else { // start is found
+                // when seq is found -> count sequence inside if
+                if (set.contains(nums[start]+incr)) { // incr= +1, +2, +3, ... so on
+                    incr++;
+                    count++;
+                } else { // when seq ends
+                    count=1;
+                    incr=1;
+                    start++;
+                }
+                max=Math.max(max,count); // updated calculation of count
+            }
+        }
+        System.out.println(max);
+    }
+    static void optimal2(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int x : nums) set.add(x);
+        int max=0;
+        for (int num : nums) {
+            if (!set.contains(num - 1)) {
+                int current = num;
+                int count = 1;
+
+                while (set.contains(current + 1)) {
+                    current++;
+                    count++;
+                }
+
+                max = Math.max(max, count);
             }
         }
         System.out.println(max);
